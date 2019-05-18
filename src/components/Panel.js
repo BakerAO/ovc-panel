e9926a9fc950996922cc3cdfba87d3415d20b9ea
BakerAO <baker.ousley@gmail.com>
2019-05-18 11:00:59 -0500

    Start

---
+++ src/components/Panel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Panel extends React.Component {
+    render() {
+        return (
+            <div>
+                Panel
+            </div>
+        );
+    };
+}
