807a44fa12517168a0d95617b74a13756b555715
BakerAO <dpshunter@gmail.com>
2019-06-05 13:27:22 -0500

    Update Times

--- client/src/components/Times.js
+++ client/src/components/Times.js
@@ -2,6 +2,11 @@ import React from 'react';
 import axios from '../apis/db';
 
 export default class Times extends React.Component {
+    componentDidMount() {
+        let times = axios.get('/times');
+        console.log(times);
+    }
+
     render() {
         return (
             <div>
