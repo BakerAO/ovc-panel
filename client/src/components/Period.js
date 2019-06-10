e7b7adcbad0a3d3fb6535b6a01e02d0c816f246f
BakerAO <dpshunter@gmail.com>
2019-06-10 13:43:25 -0500

    update

--- client/src/components/Period.js
+++ client/src/components/Period.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default class Period extends React.Component {
     render() {
         return (
-            <div>
-                <button onClick={() => this.props.handleClick()}>
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-warning"
+                    type="button"
+                    onClick={() => this.props.handleClick()}
+                >
                     Change Time Period
                 </button>
             </div>
--- client/src/components/Times.js
+++ client/src/components/Times.js
@@ -5,21 +5,33 @@ export default class Times extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            times: []
+            times1: [],
+            times2: []
         }
     }
 
     getTimes = async () => {
-        let uri = '/times1';
-        if (this.props.period === 2) {
-            uri = '/times2';
-        }
-        const response = await axios.get(`${uri}`);
-        this.setState({ times: response.data });
+        const response1 = await axios.get('/times1');
+        this.setState({ times1: response1.data });
+        const response2 = await axios.get('/times2');
+        this.setState({ times2: response2.data });
     }
 
-    renderTimes = () => {
-        return this.state.times.map((time) => {
+    renderTimes1 = () => {
+        return this.state.times1.map((time) => {
+            return (
+                <div className="p-2"
+                    key={time}
+                    style={{ border: "1px solid black" }}
+                >
+                    {time}
+                </div>
+            );
+        });
+    }
+
+    renderTimes2 = () => {
+        return this.state.times2.map((time) => {
             return (
                 <div className="p-2"
                     key={time}
@@ -36,7 +48,11 @@ export default class Times extends React.Component {
     }
 
     render() {
-        let times = this.renderTimes();
+        let times = this.renderTimes1();
+        if (this.props.period === 2) {
+            times = this.renderTimes2();
+        }
+
         return (
             <div className="d-flex flex-column">
                 {times}
